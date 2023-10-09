21a33f5505569bda9e60a662b4b0ec6e9d71451b
Mohammed Ramzi <thefallendemon10050@gmail.com>
2023-10-09 19:29:50 +0300

    initial commit

---
+++ public/js/main.js
@@ -0,0 +1,176 @@
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+
+// Agora credentials 
+const APP_ID = "4a995bb5734d4985863e32fd7931a918";
+const TOKEN =
+  "007eJxTYOAJMnqQVXTu2b9w/rDJiap+7dH1gn8WSa153rrHvXqGkaQCg0mipaVpUpKpubFJiomlhamFmXGqsVFairmlsWGipaFF0E7l1IZARobItw+ZGRkgEMRnYchNzMxjYAAA7P4eMA==";
+const CHANNEL = "main";
+// To use vid and voice
+const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+
+let localTracks = [];
+let remoteUsers = {};
+// To show stream on your end
+let joinAndDisplayLocalStream = async () => {
+  
+  client.on('user-published', handleUserJoined)
+  client.on('user-left', handleUserLeft)
+
+  let UID = await client.join(APP_ID, CHANNEL, TOKEN, null);
+
+  localTracks = await AgoraRTC.createMicrophoneAndCameraTracks();
+
+  let player = `<div class="video-container" id="user-container-${UID}">
+  <div class="video-player" id="user-${UID}"></div>
+  </div>`
+  document
+    .getElementById("video-streams")
+    .insertAdjacentHTML("beforeend", player);
+
+  localTracks[1].play(`user-${UID}`);
+
+  await client.publish([localTracks[0], localTracks[1]]);
+};
+
+let joinStream = async () => {
+  await joinAndDisplayLocalStream();
+  document.getElementById("join-btn").style.display = "none";
+  document.getElementById("stream-controls").style.display = "flex";
+};
+
+let handleUserJoined = async (user, mediaType) => {
+  remoteUsers[user.uid] = user
+  await client.subscribe(user, mediaType)
+
+  if(mediaType === 'video'){
+    let player = document.getElementById(`user-container-${user.uid}`)
+    if (player != null){
+      player.remove()
+    }
+
+    player = `<div class="video-container" id="user-container-${user.uid}">
+    <div class="video-player" id="user-${user.uid}"></div>
+    </div>`
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    user.videoTrack.play(`user-${user.uid}`)
+  }
+  if (mediaType === 'audio'){
+    user.audioTrack.play()
+  }
+}
+
+
+let handleUserLeft = async (user) =>{
+  delete remoteUsers[user.uid]
+  document.getElementById(`user-container-${user.uid}`).remove()
+}
+
+let leaveAndRemoveLocalStream = async () => {
+  for(let i = 0; localTracks.length > i; i++){
+    localTracks[i].stop()
+    localTracks[i].close()
+  }
+
+await client.leave()
+document.getElementById('join-btn').style.display = 'block'
+document.getElementById('stream-controls').style.display = 'none'
+document.getElementById('ivdeo-streams').innerHTML = ''
+
+}
+
+let toggleMic = async (e) => {
+  if (localTracks[0].muted){
+    await localTracks[0].setMuted(false)
+    e.target.innerText = 'Mic on'
+    e.target.style.backgroundColor = 'cadetblue'
+  }else{
+    await localTracks[0].setMuted(true)
+    e.target.innerText = 'Mic off'
+    e.target.style.backgroundColor = '#EE4B2B'
+  }
+}
+
+let toggleCamera = async (e) => {
+  if (localTracks[1].muted){
+    await localTracks[1].setMuted(false)
+    e.target.innerText = 'Camera on'
+    e.target.style.backgroundColor = 'cadetblue'
+  }else{
+    await localTracks[1].setMuted(true)
+    e.target.innerText = 'Camera off'
+    e.target.style.backgroundColor = '#EE4B2B'
+  }
+}
+document.getElementById("join-btn").addEventListener("click", joinStream);
+document.getElementById("leave-btn").addEventListener("click", leaveAndRemoveLocalStream);
+document.getElementById("mic-btn").addEventListener("click", toggleMic);
+document.getElementById("camera-btn").addEventListener("click", toggleCamera);
+
+
+// CHAT ROOM CODE BELOW
+//-----------------------------------------------------------------------------------------------------------------------------------
+// get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
+
+const socket = io();
+
+//Join chatroom
+socket.emit("joinRoom", { username, room });
+
+// Get room and users
+socket.on("roomUsers", ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// Message from server
+socket.on("message", (message) => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down in messages
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  // Get message text
+  const msg = e.target.elements.msg.value;
+
+  // Emit message to server
+  socket.emit("chatMessage", msg);
+
+  // Clear input
+  e.target.elements.msg.value = "";
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement("div");
+  div.classList.add("message");
+  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+  document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add room ame to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map((user) => `<li>${user.username}</li>`).join("")}`;
+}
